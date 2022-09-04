950339538e08fb7e39645fedaf8f67c65550d1ad
Dzhaner Hasan <dzhanerrhasan@gmail.com>
2022-09-04 19:36:04 +0300

    Fixed some bugs

--- finalProject/settings.py
+++ finalProject/settings.py
@@ -79,14 +79,11 @@ WSGI_APPLICATION = 'finalProject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'traveling_journal_db',
-        'USER': 'postgres',
-        'PASSWORD': 'aqwsaqws12',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
